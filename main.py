23f2d9856ccc8d9a14826dc530e71cb57814c223
GiladLeef <96906027+GiladLeef@users.noreply.github.com>
2025-03-09 12:52:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-# compiler.py
+# main.py
 import sys
 import os
 from lexer import lex
 from parser import Parser
 from codegen import CodeGen
-from codegen import compile_module
+from llvmlite import binding as llvm
+import subprocess
+
+def compile_module(llvm_module, output_exe):
+    llvm.initialize()
+    llvm.initialize_native_target()
+    llvm.initialize_native_asmprinter()
+    llvm_ir = str(llvm_module)
+    mod = llvm.parse_assembly(llvm_ir)
+    mod.verify()
+    target = llvm.Target.from_default_triple()
+    target_machine = target.create_target_machine()
+    obj_code = target_machine.emit_object(mod)
+    obj_filename = "output.o"
+    with open(obj_filename, "wb") as f:
+        f.write(obj_code)
+    bc_filename = "output.bc"
+    with open(bc_filename, 'w') as f:
+        f.write(str(llvm_module))
+    linked_bc_filename = "linked.bc"
+    subprocess.run(['llvm-link', bc_filename, '-o', linked_bc_filename], check=True)
+    subprocess.run(['clang++', linked_bc_filename, '-o', output_exe, '-lstdc++', '-lm'], check=True)
+    os.remove(obj_filename)
+    os.remove(bc_filename)
+    os.remove(linked_bc_filename)
+    print(f"Executable '{output_exe}' generated.")
+
 
 def compile_generated_module(llvm_module, output_exe):
     compile_module(llvm_module, output_exe)
