ca4359150b2571cf8b81692c9ceb19add6660327
TrollMii <trollmii@outlook.de>
2025-04-29 17:38:00 +0200

    added get-c+.sh
    This file can be downloaded and c+ will automaticly installed

---
+++ get-c+.sh
@@ -0,0 +1,16 @@
+
+
+URL="https://github.com/TrollMii/CP"
+OUTPUT_DIR="/home/$USER/.cplang"
+OUT_LINK="/home/$USER/.local/bin/c+"
+install()
+{
+    rm -rf $OUTPUT_DIR
+    rm $OUT_LINK
+    git clone $URL $OUTPUT_DIR
+    export CP_DIR=$OUTPUT_DIR
+    ln $OUTPUT_DIR/bin/c+.sh $OUT_LINK
+}
+install
+echo "Successfully installed"
+echo "Use 'c+' for compiling files"
